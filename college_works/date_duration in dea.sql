b35b4fccd0f8a470247318f1dbbccb2d7e172696
Rahul kanjariya <93303230+KanjariyaRahul@users.noreply.github.com>
2023-08-31 12:41:20 +0530

    Add files via upload

---
+++ college_works/date_duration in dea.sql
@@ -0,0 +1,19 @@
+SELECT * FROM DUAL;
+
+SELECT 2*16 from DUAL;
+
+SELECT (2+4)/2*16 from DUAL;
+
+SELECT (2*5-3)/2+16 from DUAL;
+
+SELECT to_date('17-aug-2023') from dual;
+
+SELECT to_date('17-aug-2023')+2 from dual;
+SELECT to_date('17-aug-2023')-3 from dual;
+
+SELECT to_date('17-aug-2023')+395 from dual;
+
+SELECT to_date('17-aug-2023')- to_date('15-aug-2023') from dual;
+SELECT ((to_date('17-aug-2023')- to_date('22-feb-2003'))/30)/12 from dual;
+
+SELECT ((to_date('17-aug-2023')- to_date('22-feb-2003'))/30)/12 from dual;
